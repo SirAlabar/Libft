561ca09a2bac7c1960f72a6b0d6ff1f538af87b2
Hugo Leonardo Luiz Marta <hluiz-ma@c2r5s3.42porto.com>
2024-04-21 21:06:47 +0100

    add ft_memchr

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 12:04:59 by hluiz-ma          #+#    #+#             */
-/*   Updated: 2024/04/14 19:40:36 by hluiz-ma         ###   ########.fr       */
+/*   Updated: 2024/04/21 18:27:30 by hluiz-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,16 @@
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+    unsigned int i;
+
+    i = 0;
+        while(i < n)
+        {
+            if(((unsigned char *)s)[i] == (unsigned char)c)
+                    return ((void *)(s + i));      
+            i++;
+        } 
+    return (0);
+}
