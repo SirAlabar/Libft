d2d4086e6c8fd56174121614880b4ada9872b558
Hugo Leonardo Luiz Marta <hluiz-ma@c2r5s3.42porto.com>
2024-04-21 21:07:37 +0100

    add ft_memmove

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 12:04:16 by hluiz-ma          #+#    #+#             */
-/*   Updated: 2024/04/14 12:28:27 by hluiz-ma         ###   ########.fr       */
+/*   Updated: 2024/04/21 18:57:18 by hluiz-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,28 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
+    unsigned int i;
+
+    if(!dst && !src)
+        return (0);
+    i = 0;
+    if((size_t)dst - (size_t)src < len)
+    {
+        i = len - 1;
+        while(i < len)
+        {
+            ((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+            i--;
+        }
+    }
+    else
+    {
+        while(i < len)
+        {
+            ((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+            i++;
+        }
+        
+    }
+    return (dst); 
+}
