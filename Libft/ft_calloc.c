c028cca97bc3f6819b55c31c2a1e4ccafa932ac4
Hugo Leonardo Luiz Marta <hluiz-ma@c2r5s3.42porto.com>
2024-04-21 21:06:26 +0100

    add ft_calloc

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 12:02:56 by hluiz-ma          #+#    #+#             */
-/*   Updated: 2024/04/14 12:28:27 by hluiz-ma         ###   ########.fr       */
+/*   Updated: 2024/04/21 17:34:07 by hluiz-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,19 @@
 #include "libft.h"
 
 void	*ft_calloc(size_t count, size_t size)
-{
+{  
+    size_t tot_size;
+    void *str;
+
+    if (count == 0 || size == 0)
+    {
+        count = 1;
+        size = 1;
+    }
+    tot_size = count * size;
+    if(!(str = malloc(tot_size)))
+        return(0);
+    else    
+        ft_bzero(str, tot_size);
+    return (str);
+}
