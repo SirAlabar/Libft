a66f961d3650ffcf3b4912292e27f7ffbddf1937
Hugo Leonardo Luiz Marta <hluiz-ma@c2r6s4.42porto.com>
2024-05-05 17:30:04 +0100

    att norminette

--- Libft/ft_lstadd_back_bonus.c
+++ Libft/ft_lstadd_back_bonus.c
@@ -22,13 +22,13 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-    t_list *last;
+	t_list	*last;
 
-    if(!(*lst))
-    {
-        *lst = new;
-        return;
-    }
-    last = ft_lstlast(*lst);
-    last->next = new;       
+	if (!(*lst))
+	{
+		*lst = new;
+		return ;
+	}
+	last = ft_lstlast(*lst);
+	last->next = new;
 }
