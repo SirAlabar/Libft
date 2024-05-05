407c74b60d76d957a4472da151c438bf06336eca
Hugo Leonardo Luiz Marta <hluiz-ma@c2r6s4.42porto.com>
2024-05-05 17:31:25 +0100

    att norminette

--- Libft/ft_lstiter_bonus.c
+++ Libft/ft_lstiter_bonus.c
@@ -23,12 +23,12 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-    t_list *tmp;
+	t_list	*tmp;
 
-    tmp = lst;
-    while(tmp)
-    {
-        f(tmp->content);
-        tmp = tmp->next;
-    }    
+	tmp = lst;
+	while (tmp)
+	{
+		f(tmp->content);
+		tmp = tmp->next;
+	}
 }
