e90e68667cff2232713b5b2cfa1f430440816f03
Hugo Leonardo Luiz Marta <hluiz-ma@c2r6s3.42porto.com>
2024-05-07 21:07:31 +0100

    rebuild calloc

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 12:02:56 by hluiz-ma          #+#    #+#             */
-/*   Updated: 2024/04/29 21:40:53 by hluiz-ma         ###   ########.fr       */
+/*   Updated: 2024/05/07 19:09:39 by hluiz-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,10 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	size_t	tot_size;
 	void	*str;
 
-	if (size > ((size_t)-1) / count)
-		return (NULL);
-	tot_size = count * size;
-	str = malloc(tot_size);
-	if (!str)
-		return (NULL);
-	else
-		ft_bzero(str, tot_size);
+	str = malloc(size * count);
+	if (str)
+		ft_bzero(str, count * size);
 	return (str);
 }
