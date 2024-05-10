535ad21227f303dd045f27d92979a9e419210cff
Markos Vinicius <markoscomks@gmail.com>
2024-05-10 21:06:39 +0100

    Update ft_memmove.c
    Removed nonsense if statement

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -29,15 +29,6 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	if (!dst && !src)
 		return (NULL);
 	i = 0;
-	if ((size_t)dst - (size_t)src < len)
-	{
-		i = len - 1;
-		while (i < len)
-		{
-			((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
-			i--;
-		}
-	}
 	else
 	{
 		while (i < len)
